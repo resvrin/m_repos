15bdabf4c4550eef63608856a5e3c759095465e6
resvrin <42444647+resvrin@users.noreply.github.com>
2020-12-16 18:05:47 +0530

    Update remote-state.tf

--- remote-state.tf
+++ remote-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "madhuinstancev2"
-    key  = "terraform/state"
+    key  = "state/remote-state.tf"
     region = "us-east-2"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
